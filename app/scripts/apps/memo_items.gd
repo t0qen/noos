d616bb053a92dea4139037ceb0cddeee48dd64d1
𝒅𝒓𝒆𝒂𝒎𝒔 <mybeautifuladress@gmail.com>
2025-08-28 11:12:53 +0200

    get_next_date(date, day_to_add)

--- app/scripts/apps/memo_items.gd
+++ app/scripts/apps/memo_items.gd
@@ -3,6 +3,15 @@ extends Control
 var from 
 var last
 
+
+var memo_interval = {1: 1, 2: 7, 3: 30, 4: 120}
+var current_memo_interval : int = 1
+var next_memo
+
+
+var months = {1: 31, 2: 30, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
+
+
 #var study_interval = {1 (premiere fois) : 2 (combien jour), 2 : 4} 
 #var current_interval = 2 (will be saved)
 #
@@ -12,17 +21,100 @@ var last
 #	.text = "dans {output} jours"
 #
 
+func _ready() -> void:
+	print('beghan')
+	var date = [get_current_day(), get_current_month(), get_current_year()]
+	print(date)
+	next_memo = get_next_date(date, 134) 
+	print(next_memo)
 
 func _on_kill_pressed() -> void:
 	queue_free()
 
-func set_contents(name : String, from_value : String):
+func set_contents(name : String, from_value : String, from_fromated):
 	$name.text = name
 	$from.text = from_value
-	from = from_value
+	from = from_fromated
 	
 func get_last(): # last time we had to study this
 	pass
 	
 func calculate_next():
+	var date = [get_current_day(), get_current_month()]
+	next_memo = get_next_date(date, memo_interval.get(current_memo_interval)) 
+	
+	
+func get_next_date(date, days_to_add):
+	var output = [date[0] + days_to_add, date[1], date[2]]
+	#if output[0] > months.get(date[1]):
+		#print(months.get(date[1]))
+		#output[0] = output[0] - months.get(date[1])
+		
+
+	var i : int = 0
+	while output[0] > months.get(date[1] + i): # 65 > 31 + 0 # 34 > 30
+		print("------------------------------")
+		print("i ", i)
+		print("[0]before ", output[0])
+		
+		output[0] = output[0] - months.get(date[1] + i) # 65 - 31 + 0 = 34 [34, ] # 34 - 30 = 3
+		print("[0]after ", output[0])
+		print("[1]after ", date[1])
+		output[1] = date[1] + (1 + i) # [34, 9] # [3, 10]
+		if output[1] > 12:
+			output[1] = 1
+			date[1] = 1
+			output[2] = output[2] + 1
+		print("[1]after ", output[1])
+		i = i + 1 # 2
+		
+		
+			
+		print(output)	
+		print("------------------------------")
+
+	
+	return output
+	
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+func get_latest():
 	pass
+	
+#func additionate_date(var ):
+	
+	
+	
+func get_current_month():
+	var datetime = Time.get_datetime_dict_from_system()
+	return datetime.month
+	
+func get_current_day():
+	var datetime = Time.get_datetime_dict_from_system()
+	return datetime.day
+	
+func get_current_year():
+	var datetime = Time.get_datetime_dict_from_system()
+	return datetime.year
+	
+	
+	
+	
+	
+	
--- app/scripts/apps/memo_reminder.gd
+++ app/scripts/apps/memo_reminder.gd
@@ -9,17 +9,19 @@ func add_item(name : String):
 	var current_item = item.instantiate()
 	$ScrollContainer/VBoxContainer.add_child(current_item)
 	
-
+	
 	# current date
 	var datetime = Time.get_datetime_dict_from_system()
-	var output : String = str(datetime.day) + "/" + str(datetime.month)
+	var output : String = str(datetime.day) + "/" + str(datetime.month) + "/" + str(datetime.year)
 		
-	current_item.set_contents(name, output)
+	current_item.set_contents(name, output, [datetime.day, datetime.month, datetime.year])
 
 func _on_add_pressed() -> void:
-	add_item($input.text)
-	$input.text = ""
+	if $input.text != "":
+		add_item($input.text)
+		$input.text = ""
 
 func _on_input_text_submitted(new_text: String) -> void:
-	add_item(new_text)
-	$input.text = ""
+	if $input.text != "":
+		add_item(new_text)
+		$input.text = ""
